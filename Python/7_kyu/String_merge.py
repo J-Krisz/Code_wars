0d9af5c86c0877c1c67fa62739e642695e1b474f
J-Krisz <jkristi4n@gmail.com>
2022-01-20 17:07:13 +0000

    7_kyu

---
+++ Python/7_kyu/String_merge.py
@@ -0,0 +1,9 @@
+# Given two words and a letter, return a single word that's a combination of
+# both words, merged at the point where the given letter first apprears in each
+# word. The returned word should have the beginning of the first word and the
+# ending of the second, with the dividing letter in the middle. You can assume
+# both words will contain the dividing letter.
+
+def string_merge(string1, string2, letter):
+    return string1[:string1.index(letter)] + string2[string2.index(letter):]
+
