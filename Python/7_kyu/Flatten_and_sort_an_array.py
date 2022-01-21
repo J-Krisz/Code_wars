1206aa97ee8af9ee4009434e351500c31855f7c8
J-Krisz <jkristi4n@gmail.com>
2022-01-21 17:36:51 +0000

    details

--- Python/7_kyu/Flatten_and_sort_an_array.py
+++ Python/7_kyu/Flatten_and_sort_an_array.py
@@ -1,2 +1,11 @@
+# Challenge:
+    
+    # Given a two-dimaensional array of integers, return the flattened version
+    # of the array with all the integers in the sorted(ascending) order.
+
+    # Example: 
+    # Given [[3,2,1], [4,6,5], [], [9, 7, 8]], you function should return
+    # [1,2,3,4,5,6,7,8,9]
+
 def flatten_and_sort(array):
     return sorted([i for l in array for i in l])
